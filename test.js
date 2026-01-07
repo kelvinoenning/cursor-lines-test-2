c680520d9fa6155b2863a3d731d03ea05c5e13f1
Kelvin Oenning <kelvinstang@hotmail.com>
2026-01-07 17:52:50 -0300

    test

---
+++ test.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a + b;
+}
+
+console.log(sum(1, 2));
+console.log(subtract(4, 1));
+console.log(divide(10, 2));
+console.log(multiply(3, 4));
