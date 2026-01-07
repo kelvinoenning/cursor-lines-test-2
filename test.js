8d8677ac09d16748f8d1c503df670c49b2834f6a
Kelvin Oenning <kelvinstang@hotmail.com>
2026-01-07 17:59:28 -0300

    test

--- test.js
+++ test.js
@@ -6,6 +6,14 @@ function subtract(a, b) {
     return a + b;
 }
 
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
 console.log(sum(1, 2));
 console.log(subtract(4, 1));
 console.log(divide(10, 2));
